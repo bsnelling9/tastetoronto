25af1c14606bd934b976af5e1063ef41f1d72071
Brodie <brodiesnelling@gmail.com>
2022-06-08 12:06:49 -0400

    first commit

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,18 @@
+class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: %i[ show edit update destroy ]
+
+  # GET /blog_posts or /blog_posts.json
+  def index
+    api = Prismic.api('https://blog-webs.cdn.prismic.io/api/v1')
+    response = api.query(Prismic::Predicates.at("document.type", "brodie"), { "orderings" => "[my.blog-post.date desc]" })
+    @documents = response.results
+    logger.info(@documents)
+  end
+
+  def show
+    api = Prismic.api('https://blog-webs.cdn.prismic.io/api/v1')
+    response = api.query(Prismic::Predicates.at("document.ud", "1"),{ "lang" => "*" })
+    document = response.results[0]
+  end
+
+end
