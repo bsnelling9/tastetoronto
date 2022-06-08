5c9551b4ffcb71e6eba2441a1c6dc26a410a716c
Brodie <brodiesnelling@gmail.com>
2022-06-08 15:57:16 -0400

    updated code

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -6,13 +6,15 @@ class BlogPostsController < ApplicationController
     api = Prismic.api('https://blog-webs.cdn.prismic.io/api/v1')
     response = api.query(Prismic::Predicates.at("document.type", "brodie"), { "orderings" => "[my.blog-post.date desc]" })
     @documents = response.results
-    
+    logger.info(@documents)
+ 
   end
 
   def show
+  	@blog_post = blog_post.find(params[:id])
     api = Prismic.api('https://blog-webs.cdn.prismic.io/api/v1')
-    response = api.query(Prismic::Predicates.at("document.id", "1"),{ "lang" => "*" })
-    document = response.results[0]
+    response = api.query(Prismic::Predicates.at("document.id", id),{ "lang" => "*" })
+    post = response.results[0]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :id
   # Defines the root path route ("/")
   # root "articles#index"
   root "blog_posts#index"
+
+  get '/id', to: 'blog_post#show'
 end
