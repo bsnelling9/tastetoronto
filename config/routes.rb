ebad4f28b85f20a2e46988ea7b7d93b99d371196
Brodie <brodiesnelling@gmail.com>
2022-06-08 16:02:31 -0400

    fixed code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :id
+
   # Defines the root path route ("/")
   # root "articles#index"
   root "blog_posts#index"
 
-  get '/id', to: 'blog_post#show'
+  get '/blog_posts/:id', to: 'blog_posts#show'
 end
